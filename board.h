a204a40c0d971595e2d18ef3518e93c95ed89c99
Artsemi Babounikau <62169998+volchonok091@users.noreply.github.com>
2022-05-13 17:57:02 +0300

    Update board.h

--- board.h
+++ board.h
@@ -1,11 +1,5 @@
 #pragma once
 
-/*
-	This class provides the basic functions for the connect4 such as the 2d array to hold all of the values,
-	the functions to print out the board and check if there is a winner. It doesn't actually set up the board
-	and a seperate class is needed to set up the dimensions, what is required to win (4 in a row, 5 in a row
-	etc) and what characters are used for player 1 and 2.
-*/
 
 class Board
 {
@@ -14,9 +8,8 @@ private:
 	int height, width;
 	char player1, player2;
 	int win;
-	///int retNumDigits(int in) { int nums = 0; do { in = in / 10; nums++; } while (in != 0); return nums; }
 public:
-	//Constructor & Destructor (Main constructor done in Implementation class)
+	//Constructor & Destructor 
 	Board() {} 
 	~Board() { for (int i = 0; i < width; i++)delete[] place[i]; delete[] place; }
 
